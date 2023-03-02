4600777b1f7ba47a7fae42bbb8a8085cee33f10a
virusal7rbi <fahadalwasidi@gmail.com>
2023-03-02 20:37:50 +0300

    telephony pack, ANDROID ONLY

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'dart:convert';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:awesome_notifications/awesome_notifications.dart';
@@ -6,26 +8,26 @@ import 'package:telephony/telephony.dart';
 
 void messageHandler(SmsMessage message) {
   if (message.body.toString().contains("شراء")) {
-    LineSplitter ls = const LineSplitter();
-    List lines = ls.convert(message.body.toString());
-    String price = lines[3].toString().split(":")[1];
+  LineSplitter ls = const LineSplitter();
+  List lines = ls.convert(message.body.toString());
+  String price = lines[3].toString().split(":")[1];
 
-    //show notification
-    AwesomeNotifications().createNotification(
-        content: NotificationContent(
-            id: 123,
-            channelKey: 'payment_amount',
-            title: 'payment amount',
-            body: price,
-            category: NotificationCategory.Message));
+  //show notification
+  AwesomeNotifications().createNotification(
+      content: NotificationContent(
+          id: 123,
+          channelKey: 'payment_amount',
+          title: 'payment amount',
+          body: price,
+          category: NotificationCategory.Message));
   }
 }
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   // set variable
-  Telephony telephony = Telephony.instance;
+  final Telephony telephony = Telephony.instance;
   // check permissions
   await Permission.notification.isDenied.then((value) {
     if (value) {
@@ -70,24 +72,23 @@ void main() async {
   // show notification to keep app on background
   AwesomeNotifications().createNotification(
       content: NotificationContent(
-          id: 123,
+          id: 1234,
           channelKey: 'payment_amount_keep_background',
           title: 'payment amount',
           body: "this notification is show to keep the app running",
           locked: true,
           autoDismissible: false,
           actionType: ActionType.SilentAction,
-          showWhen: false,
           notificationLayout: NotificationLayout.BigText,
+          
           category: NotificationCategory.Service));
 
+
+  runApp(const MyApp());
   // set sms listener
   telephony.listenIncomingSms(
-    onNewMessage: messageHandler,
-    onBackgroundMessage: messageHandler,
-  );
-  runApp(const MyApp());
-
+      onNewMessage: messageHandler,
+      onBackgroundMessage: messageHandler);
 }
 
 class MyApp extends StatelessWidget {
